2841a5cf4c011d19858e4c77bb8b8a8a9926fee8
takur-ryu <takuryu.1049@icloud.com>
2020-09-21 14:16:24 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,14 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
+      @messages = @room.messages.includes(:user)
       redirect_to room_messages_path(@room)
     else
       render :index
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  belong_to :room
-  belong_to :user
+  belongs_to :room
+  belongs_to :user
 
 end
